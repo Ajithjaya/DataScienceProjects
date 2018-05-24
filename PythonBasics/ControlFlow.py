3c7f71a601f85c5f3794b2c5bac4a8954b2c65c5
Ajithjaya <ajith30022@gmail.com>
2018-05-24 19:56:08 +0530

    Class on 23 and 24 may

--- PythonBasics/ControlFlow.py
+++ PythonBasics/ControlFlow.py
@@ -32,33 +32,120 @@ for i in range(len(a)):
     print(i, a[i])
 
 # Break statement
-for n in range(2,10):
-    for x in range (2, n):
+for n in range(2, 10):
+    for x in range(2, n):
         if n % x == 0:
-            print (n, 'equals',x, '*', n//x)
+            print(n, 'equals', x, '*', n // x)
             break
         else:
-            print (n, 'is a prime number')
+            print(n, 'is a prime number')
             break
 
 # Continue statement
 for num in range(2, 10):
-    if num % 2== 0:
+    if num % 2 == 0:
         print("Found an even number", num)
         continue
     print("Found an odd number", num)
 
+
 # pass statement does nothing. it can be used when a statement is required syntactically but program requires no action
 def initlog(*args):
     pass
+
 
 def fib2(n):
     """ Return alist containing the Fibionacci series upto n"""
     result = []
     a, b = 0, 1
     while a < n:
-         result.append(a)
-         a, b = b, a+b
+        result.append(a)
+        a, b = b, a + b
     return result
+
+
 f100 = fib2(100)
 print(f100)
+
+
+# Default argument values
+def ask(prompt, retries=4, reminder='please try again!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
+
+
+#  ask('Do you really want to quit?', 5)
+#  ask('OK to overwrite the file?', 2, 'come on, only yes or no!')
+
+# keyword arguments
+def parrot(voltage, state='a stiff'):
+    print(voltage, state)
+
+
+#  parrot(voltage=1000,state='dfd')
+#  after a keyword argument non keyword argument throws error
+parrot(1000, 'dfd')
+
+
+def cheese(kind, *arguments, **keywords):  # * - list, ** - dictionary and ** must always come at last
+    print(kind)
+    for arg in arguments:
+        print('tuple values : ', arg)
+    for w in keywords:
+        print('dictionary values :', keywords[w])
+
+
+cheese('MY name is Ajith', "first argument in tuple", "second argument in tuple ", language="python",
+       technology='Deep Learning')
+
+
+# Arbitrary arguments list
+def concat(*args, sep="/"):
+    return sep.join(args)
+
+
+print(concat("earth", "mars", "venus"))
+
+# Unpackaging Arguments Lists
+print(list(range(3, 6)))  # normal call with seperate arguments
+args = [3, 6]
+print(list(range(*args)))  # call with arguments unpacked from a list
+
+
+# In the same fashion dictionaries can deliver keyword arguments
+#  with hte **-operator:
+def parrot(voltage, state='a stiff',
+           action='voom'):  # in case of a method defined two types it gets overloaded i.e method with most argumets get read.
+    print(state)
+    print(action)
+    print(voltage)
+
+
+d = {"voltage": "four million", "state": "bleeding demised", "action": "VOOM"}
+parrot(**d)
+
+
+# Lambda Expressions
+def make_incrementor(n):
+    return lambda x: x + n
+
+
+f = make_incrementor(42)
+print(f(0))
+print(f(1))
+
+# pass a small function as an argumnent
+pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
+pairs.sort(key=lambda pair: pair[1])
+print(pairs)
+
+# coding style
+#  https://www.python.org/dev/peps/pep-0008/   -  for styling in python code
--- PythonBasics/PythonTypes.py
+++ PythonBasics/PythonTypes.py
@@ -1,7 +1,7 @@
 from typing import List
 
 print(2 + 2)
-print((50 - 5 * 6) / 4)  # follows BODMAS rule
+print((50 - 5 * 6) / 4)  # follows BODMAS rule in implementing functions in case of only numerical variables are present
 print(8 / 5)  # division always returns a floating point number
 print(17 // 3)  # floor division discards fractional part
 print(17 % 3)  # the % operator returns remainder of division
@@ -10,7 +10,7 @@ print(2 ** 7)  # 2 to the power of 7
 # strings
 print('welcome to python')
 print("doesn't")
-print('Firstline\nsecond line')
+print('Firstline\nSecond line')
 print(r'c:\some\name')
 print('c:\some\name')
 # By using triple quotes string literals can span multiple lines
@@ -32,34 +32,34 @@ print(prefix + 'thon')
 word = 'python'
 print(word[0])
 print(word[4])
-# If indices are given negative number, starts counting from the right
-print(word[-1])  # last digit = -1
+# If indices are given negative number, start counting from the right
+print(word[-1])   # last digit = -1
 print(word[-2])
 print(word[-6])
 # Slicing allows you to obtain substring:
-print(word[0:2])  # left number digit is included while right is excluded
+print(word[0:2])   # left number digit is included while right is excluded
 print(word[:2] + word[2:])
 # in first case left is taken as zero and in second case right is taken as last digit
-print(word[-2:])  # Digits included from second last to end
+print(word[-2:])   # Digits included from second last to end
 # Python strings cannot be changed,they are immutable
 # word[0] ='J'
 # word[2:]='py'  - above two result in error
 print('j' + word[1:])
-print(len('python'))  # len() returns length of string
+print(len('python'))   # len() returns length of string
 
 # Lists
 squares = [1, 4, 9, 16, 25]
 print(squares)
 print(squares[0])
-print(squares[-3:])  # Lists are mutable
+print(squares[-3:])   # Lists are mutable
 cubes = [1, 8, 27, 65, 125]
 cubes[3] = 64
 print(cubes)
-cubes.append(6**3) # Append adds value to the end of list
+cubes.append(6**3)    # Append adds value to the end of list
 print(cubes)
-cubes[2:2] = [20,40] #replace or add some values
+cubes[2:2] = [20,40]    # replace or add some values
 print(cubes)
-cubes[:] = [] # clears all the values with an empty list
+cubes[:] = []      # clears all the values with an empty list
 print(len(['a','b']))
 a = ['a', 'b', 'c']
 n = [1,2,3]
@@ -69,5 +69,5 @@ print(x[0])
 print(x[1][0])
 a,b = 0,1
 while b < 1000:
-    print(b, end=',')
+    print(b, end='\n')
     a,b = b,a+b
