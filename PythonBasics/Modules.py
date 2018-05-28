61cdbab9f24393303567f1af987904be6bfeaddb
Ajithjaya <ajith30022@gmail.com>
2018-05-28 19:42:07 +0530

    Class on 28 may

--- PythonBasics/Modules.py
+++ PythonBasics/Modules.py
@@ -1,8 +1,23 @@
 # Modules
-from PythonBasics.fibo import fib
+#from PythonBasics.Fibo import Fibo
 
-import fibo
+import Fibo
 
-print(fibo.fib(1000))
-print(fibo.fib2(100))
-print(fibo.__name__)
+print(Fibo.fib(1000))
+print(Fibo.fib2(100))
+print(Fibo.__name__)
+
+fib = Fibo.fib
+print(fib(500))
+
+from Fibo import fib, fib2
+print(fib(500))
+
+from Fibo import *
+print(fib2(500))
+
+import Fibo as xyz
+print(xyz.fib(500))
+
+from Fibo import fib as fibonacci
+print(fibonacci(500))
